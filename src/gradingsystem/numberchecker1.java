196f1e03e2bf00c136b7dd078d5b618fede5afe2
kianna365 <kiskisankian5@gmail.com>
2025-07-24 14:33:45 +0800

    I added numberchecker 3

--- src/gradingsystem/numberchecker1.java
+++ src/gradingsystem/numberchecker1.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gradingsystem;
 
-/**
- *
- * @author PC16
- */
 public class numberchecker1 {
     
+    public static void main(String[] args) {
+        
+    }
 }
